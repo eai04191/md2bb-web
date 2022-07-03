ff680916fa7870ec2a0c5f51f2752a7adb6b459d
Irie Aoi <eai@mizle.net>
2022-07-03 09:55:28 +0900

    fix ulist issue
    #1

--- src/script.ts
+++ src/script.ts
@@ -4,5 +4,12 @@ const md2bb = new Md2bb();
 document.querySelector("#convert").addEventListener("click", () => {
     const md = document.getElementById("markdown") as HTMLTextAreaElement;
     const bb = document.getElementById("bbcode") as HTMLTextAreaElement;
-    bb.value = md2bb.parse(md.value);
+
+    let result = md2bb.parse(md.value);
+    console.log(result);
+    // ulist fix
+    result = result.replace(/\[ulist\]\n/g, "[list]\n");
+    result = result.replace(/\[\/ulist\]\n/g, "[/list]\n");
+
+    bb.value = result;
 });
