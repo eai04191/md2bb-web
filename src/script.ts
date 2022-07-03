d7dceafcc923bab6550580a41b00c22dbeb22b2f
Irie Aoi <eai@mizle.net>
2022-07-03 09:57:39 +0900

    add demo text

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,36 @@
 import Md2bb from "md2bb";
 const md2bb = new Md2bb();
 
+document.addEventListener("DOMContentLoaded", () => {
+    const defaultText = `
+# Hello World!
+
+*This* is a _demo text_ for **md2bb**!
+
+> It should work fine! ~maybe!~
+
+~~If it doesn't work, please report it in an [issue](https://github.com/eai04191/md2bb-web/issues)!~~
+
+- list
+- items
+
+1. ordered
+2. list
+3. items
+
+>[Eai] I hope it helps you!
+
+
+\`
+Have a
+    nice day!
+\`
+    `.trim();
+
+    const md = document.getElementById("markdown") as HTMLTextAreaElement;
+    md.value = defaultText;
+});
+
 document.querySelector("#convert").addEventListener("click", () => {
     const md = document.getElementById("markdown") as HTMLTextAreaElement;
     const bb = document.getElementById("bbcode") as HTMLTextAreaElement;
